cf98d8bab67ea2e9f836e5ccf77aa962e8d13c3c
sidgrouse <sidgrouse@gmail.com>
2018-05-18 00:20:55 +0400

    ояварпывар

--- Kaspersky/First/SyncronizedQueue.cs
+++ Kaspersky/First/SyncronizedQueue.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Kaspersky.First
 {
-    class SyncronizedQueue
+    class SyncronizedQueue<T>
     {
+        private object _locker = new object();
+        private Queue<T> _queue = new Queue<T>();
+
+        public void Push(T item)
+        {
+            lock (_locker)
+            {
+                _queue.Enqueue(item);
+                Monitor.Pulse(_locker);
+            }
+        }
+
+        public T Pop()
+        {
+            lock (_locker)
+            {
+                if (0 == _queue.Count)
+                {
+                    Monitor.Wait(_locker);
+                }
+                return _queue.Dequeue();
+            }
+
+        }
     }
 }
