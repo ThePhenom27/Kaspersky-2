f46eb4f77a6531255df038aeaab7c2e712466442
sidgrouse <sidgrouse@gmail.com>
2018-05-18 02:07:04 +0400

    без рефакторинга

---
+++ Kaspersky/Second/CoupleBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaspersky
+{
+    class CoupleBuilder
+    {
+        private List<int> _numbers;
+        public CoupleBuilder(List<int> numbers) {
+            _numbers = numbers;
+        }
+        public CoupleBuilder(params int[] numbers) 
+        {
+            _numbers = numbers.ToList();
+        }
+        public List<Tuple<int, int>> BuildWithSum(int sum) {
+            var ret = new List<Tuple<int, int>>();
+
+            _numbers.Sort();
+
+            int iFirst, iSecond;
+            for (iFirst = 0; iFirst < _numbers.Count; iFirst++) {
+                if (_numbers[iFirst] >= sum) break;
+                for (iSecond = iFirst + 1; iSecond < _numbers.Count; iSecond++) {
+                    int currentSum = _numbers[iFirst] + _numbers[iSecond];
+                    if (currentSum == sum) {
+                        ret.Add(new Tuple<int, int>(_numbers[iFirst], _numbers[iSecond]));
+                        _numbers.RemoveAt(iSecond);
+                        _numbers.RemoveAt(iFirst);
+                        iSecond -= 2;
+                        iFirst -= 1;
+                        break;
+
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}
---
+++ Kaspersky/Second/Test.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Kaspersky.Second.Tests
+{
+    [TestFixture]
+    class CoupleBuilderTests
+    {
+        [Test]
+        public void CoupleBuilder_OrdinaryCase() {
+            CoupleBuilder cb = new CoupleBuilder(1, 1, 2, 1, 1, 0, 1);
+            var ret = cb.BuildWithSum(2);
+            Assert.AreEqual(3, ret.Count);
+            Assert.AreEqual(new Tuple<int, int>(0, 2), ret[0]);
+            Assert.AreEqual(new Tuple<int, int>(1, 1), ret[1]);
+            Assert.AreEqual(new Tuple<int, int>(1, 1), ret[2]);
+        }
+        [Test]
+        public void CoupleBuilder_TooBigNumbers()
+        {
+            CoupleBuilder cb = new CoupleBuilder(1, 1, 2, 1, 1, 0, 1,3,7,4,5);
+            var ret = cb.BuildWithSum(2);
+            Assert.AreEqual(3, ret.Count);
+            Assert.AreEqual(new Tuple<int, int>(0, 2), ret[0]);
+            Assert.AreEqual(new Tuple<int, int>(1, 1), ret[1]);
+            Assert.AreEqual(new Tuple<int, int>(1, 1), ret[2]);
+        }
+        [Test]
+        public void CoupleBuilder_NoCouples()
+        {
+            CoupleBuilder cb = new CoupleBuilder(2,4,6,8,2,0,4,2);
+            var ret = cb.BuildWithSum(5);
+            Assert.AreEqual(0, ret.Count);
+        }
+        [Test]
+        public void CoupleBuilder_SameCouples()
+        {
+            CoupleBuilder cb = new CoupleBuilder(5,2,2,5,5,2,2,5);
+            var ret = cb.BuildWithSum(7);
+            Assert.AreEqual(4, ret.Count);
+            Assert.AreEqual(new Tuple<int, int>(2, 5), ret[0]);
+            Assert.AreEqual(new Tuple<int, int>(2, 5), ret[1]);
+            Assert.AreEqual(new Tuple<int, int>(2, 5), ret[2]);
+            Assert.AreEqual(new Tuple<int, int>(2, 5), ret[3]);
+        }
+    }
+}
