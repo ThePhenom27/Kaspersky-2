a1eb3edfe88454ea15b6ed8bedb97bc1563279fa
sidgrouse <sidgrouse@gmail.com>
2018-05-18 00:16:00 +0400

    очередь

--- Kaspersky/Class1.cs
+++ Kaspersky/Class1.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace Kaspersky
+namespace Kaspersky.First
 {
-    public class Class1
+    class SyncronizedQueue<T>
     {
+        private object _locker = new object();
+        private Queue<T> _queue = new Queue<T>();
+
+        public void Push(T item)
+        {
+            lock (_locker)
+            {
+                _queue.Enqueue(item);
+                Monitor.Pulse(_locker);
+            }
+        }
+
+        public T Pop()
+        {
+            lock (_locker)
+            {
+                if (0 == _queue.Count)
+                {
+                    Monitor.Wait(_locker);
+                }
+                return _queue.Dequeue();
+            }
+
+        }
     }
 }
